f9e3c1c12ba58cf49fe234b0514e23e93a46b545
cesar.montoya <cesar.montoya@DESARROLLO-DP.LINEADATASCAN.local>
2017-12-11 20:32:03 -0500

    Clase del 11/12/2017

---
+++ Unidad4-prueba2/src/main/java/com/lds/ervlet1.java
@@ -0,0 +1,38 @@
+package com.lds;
+
+import java.io.IOException;
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ervlet1
+ */
+@WebServlet("/ervlet1")
+public class ervlet1 extends HttpServlet implements Servlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ervlet1() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String[] urlArr = System.getProperties().toString().split(",");
+		String cadena = "PROPIEDADES DEL SISTEMA<br><br><br>";
+		for (int i = 0; i < urlArr.length; i++){
+		    cadena += i + "):" + urlArr[i] + "<br>";
+		}
+        response.getWriter().print(cadena);
+	}
+}
